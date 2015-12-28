daf6d8e509514b44b39a92d338310623948199eb
xieguigang <xie.guigang@gcmodeller.org>
2015-12-29 00:21:35 +0800

    node mode

---
+++ bclusterTree/R/bin.R
@@ -0,0 +1,17 @@
+#' Create a new tree node
+#'
+#' @param x a node content data object
+#'
+#' @details the initial node content data object \code{x} is already includes into the
+#'     \code{members} slot of the created new binary tree node list object.
+#'
+binaryTree.node <- function(key, x, name) {
+  values         <- list();
+  values[[name]] <- x;
+
+  list(key     = key,
+       members = values,
+       left    = -1,
+       right   = -1
+  );
+}
