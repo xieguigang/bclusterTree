fab7408022bc4964a48b56d0c8300007f6958592
xieguigang <xie.guigang@gcmodeller.org>
2016-01-03 02:46:22 +0800

    add code comments

--- bclusterTree/R/binTree.R
+++ bclusterTree/R/binTree.R
@@ -1,3 +1,16 @@
+
+#' Build a binary tree
+#'
+#' @param compares a comparision function that compares the order of
+#'     any two elements in objects list. this function should generates
+#'     3 values for build a tree cluster: 0 for identical, means clustering
+#'     1 for great than, means append to right node and -1 means less than,
+#'     which will append to left node
+#'
+#' @param objects this parameter should be an object list.
+#' @param key a lambda function that create index key of the element object 
+#'     in the objects parameter.
+#'
 buildBTree = function(objects, key, compares) {
 	root = binaryTree.node(1);
 	
@@ -6,3 +19,4 @@ buildBTree = function(objects, key, compares) {
 		
 	}
 }
+
