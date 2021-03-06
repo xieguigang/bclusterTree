294ea569f4a6285c1713b07ec36fa23656f1602f
xieguigang <xie.guigang@gcmodeller.org>
2021-03-06 16:36:08 +0800

    create cluster based on the binary tree

--- bclusterTree/R/cluster.R
+++ bclusterTree/R/cluster.R
@@ -1,12 +1,23 @@
-bcluster = function(objects, compares) {
-    # each tree node in the created binary
-    # tree is an object cluster
-    btree = buildBTree(objects, compares);
-    keys = names(objects);
 
-      if (is.null(keys)) {
+#' Clustering the given objects
+#'
+#' @param objects a list of data objects
+#' @param compares evaluate the orders between any element object in \code{objects},
+#'     see \code{\link{buildBTree}}.
+#'
+bcluster = function(objects, compares) {
+  # each tree node in the created binary
+  # tree is an object cluster
+  btree = buildBTree(objects, compares);
+  keys = names(objects);
+
+  if (is.null(keys)) {
     keys = 1:length(objects);
   }
 
-    
+  names(btree) = sprintf("B%s", 1:length(btree));
+
+  lapply(btree, function(bin) {
+    keys[bin @ members];
+  });
 }
